c38a2d6c4501ec88f7c10bf649e500e0dce7da43
William Laurence <wlaurence03@gmail.com>
2026-07-17 11:31:27 +0200

    interact with tree

---
+++ objects/maple_tree_hittable_light.gd
@@ -0,0 +1,10 @@
+extends AnimatedSprite2D
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	play("shake")
